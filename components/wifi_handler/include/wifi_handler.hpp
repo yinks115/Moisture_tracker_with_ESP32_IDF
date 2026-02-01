1b4ccd294df8da0b3bf0997d04cd05b620f1f218
Yinka <yinka.e14@gmail.com>
2026-02-01 16:08:00 -0500

    added wifi handler. still yet to implement post_reading func

--- components/wifi_handler/include/wifi_handler.hpp
+++ components/wifi_handler/include/wifi_handler.hpp
@@ -13,6 +13,27 @@ typedef struct {
 
 extern events_data_t wifi_events;
 
+/**
+ * Set up function for creating wifi events loop and group. in addition this function sets
+ * up the wifi and ip event handlers
+ * 
+ * @return returns an esp_err_t value. if everything went well that value is ESP_OK
+ */
+esp_err_t wan_event_handler_setup();
+
+/**
+ * Registers http event handler and starts the wifi connection
+ * 
+ * @return returns esp_ok if everything was successful
+ */
+esp_err_t start_wifi_connection();
+
+/**
+ * Stops the wifi connection
+ * @return returns ESP_OK or ESP_FAIL
+ */
+esp_err_t stop_wifi_connection();
+
 /** 
 * Handler for Wifi events
 * 
@@ -42,5 +63,20 @@ void ip_event_handler(
     int32_t event_id,
     void* event_data //will be NULL
 );
+
+/**
+* Handler for http events
+* @param event http event object
+* @return esp_err_t returns ESP_OK on success, ESP_FAIL if an error occured, ESP_ERR_INVALID_RESPONSE if event id in event object is invalid
+* 
+*/
+esp_err_t http_event_handler(esp_http_client_event_t* event);
+
+/**
+ * returns the mininum of 2 numbers
+ * @param n1 first number
+ * @param n2 2nd number
+ */
+int _min(int n1, int n2);
 
 #endif
